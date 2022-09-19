197323c57106d82164af012e3415bf23840ce295
jetblacksavior <coolanimeguy666@gmail.com>
2022-09-18 20:06:58 -0600

    Add project files.

---
+++ turt/turt.py
@@ -0,0 +1,27 @@
+import turtle
+walk = turtle.Turtle()
+
+def equilateral(radius:int, vertices:int):
+    #create a new turtle
+    if vertices <3 :
+        print("has to be bigger/equal to 3")
+        return
+    temp_tert = turtle.Turtle()
+    angle = ((180*(vertices-3))+180)/vertices#find angle given vertices 
+    if angle == 60:
+        print("is ")
+    for i in range(vertices):
+        temp_tert.forward(radius)
+        temp_tert.right(angle)
+        
+
+    return
+
+
+
+
+
+
+equilateral(50,3)
+     
+turtle.done()
